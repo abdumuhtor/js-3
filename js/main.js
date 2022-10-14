3439335df714203461aac8031c869e4835465b1b
abdumuhtor <abdumuhtor2262gmail.com>
2022-10-13 21:28:52 -0700

    initial commit

---
+++ js/main.js
@@ -0,0 +1,20 @@
+/* var a = 9
+
+ if(a <= 18){
+    console.log('ukam qalesan');
+ }else if(a == 18){
+    console.log('dostim qalesiz');
+ }else if(a >= 20) {
+    console.log('aka qalesiz');
+ }else{
+    console.log('xatolik');
+ } */
+ 
+ alert('Assalomu alaykum')
+ var name = prompt('Ismingizni kiriting!')
+ var age = prompt('Yoshingizni kiriting!')
+ var num = prompt('Telefon raqamizi kiriting!')
+
+ console.log(name);
+ console.log(age);
+ console.log(num);
